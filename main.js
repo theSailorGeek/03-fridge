984baadca686128fe2d78d09bf10c49367d04511
theSailorGeek <63855396+theSailorGeek@users.noreply.github.com>
2020-04-26 15:00:57 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,45 @@
 const apilink = "https://api.spoonacular.com/recipes/findByIngredients?";
 const apikey = "&apiKey=";
-var ingredients = "ingredients=porc,+cheese,+potato"
-var results = "&number=2"
+var ingredients = "ingredients="
+var results = "&number=3"
 const license = "&limitLicense=true"
 
 fridge = []
 
-var apirequest = apilink + ingredients + license + results + apikey
+
 
 
 function request() {
-    console.log("Here we go")
+    
+    for (var i = 0; i < (fridge.length - 1); i++) {
+        ingredients = ingredients + fridge[i] + ",+";
+    }
+    ingredients = ingredients + fridge[fridge.length -1];
+    
+    const apirequest = apilink + ingredients + license + results + apikey;
     fetch(apirequest)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
         const title1 = data[0].title;
+        const image1 = data[0].image;
         
-        console.log(data);
+        const title2 = data[1].title;
+        const image2 = data[1].image;
+        
+        const title3 = data[2].title;
+        const image3 = data[2].image;
+        
+        document.getElementById('image1').src = image1;
+        document.getElementsByTagName('h3')[0].innerHTML = title1;
+
+        document.getElementById('image2').src = image2;
+        document.getElementsByTagName('h3')[1].innerHTML = title2;
+
+        document.getElementById('image3').src = image3;
+        document.getElementsByTagName('h3')[2].innerHTML = title3;
+       
     });
 
 }
